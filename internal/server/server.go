5883d175e9de7cb86001f8ecb86fa51d07bb17b2
SergeyPanov <panovsy@gmail.com>
2026-06-06 23:20:16 +0200

    alaborated on http server

---
+++ internal/server/server.go
@@ -0,0 +1,74 @@
+package server
+
+import (
+	"log"
+	"net"
+	"strconv"
+	"sync"
+	"sync/atomic"
+
+	"github.com/SergeyPanov/httpfromtcp/internal/request"
+)
+
+type Server struct {
+	listener net.Listener
+	closed   atomic.Bool
+	wg       sync.WaitGroup
+}
+
+func Serve(port int) (*Server, error) {
+	p := strconv.Itoa(port)
+	l, err := net.Listen("tcp", ":"+p)
+
+	if err != nil {
+		return nil, err
+	}
+
+	server := &Server{listener: l, closed: atomic.Bool{}, wg: sync.WaitGroup{}}
+
+	server.wg.Go(func() {
+		server.listen()
+	})
+
+	return server, nil
+}
+
+func (s *Server) listen() {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			if s.closed.Load() {
+				return
+			}
+			log.Println(err)
+			continue
+		}
+
+		s.wg.Go(func() {
+			s.handle(conn)
+		})
+	}
+}
+
+func (s *Server) handle(conn net.Conn) {
+	defer conn.Close()
+
+	resp := "HTTP/1.1 200 OK\r\n" +
+		"Content-Type: text/plain\r\n" +
+		"Content-Length: 13\r\n" +
+		"\r\n" +
+		"Hello World!"
+
+	request.RequestFromReader(conn)
+
+	// fmt.Println(resp)
+	conn.Write([]byte(resp))
+
+}
+
+func (s *Server) Close() error {
+	s.closed.Store(true)
+	err := s.listener.Close()
+	s.wg.Wait()
+	return err
+}
